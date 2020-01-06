6233ac3241ac2b215d7acebbb9c3c2afee9d276a
M4st3rH4ck3r <57261873+M4st3rH4ck3r@users.noreply.github.com>
2020-01-06 13:47:14 +0200

    added new autonomous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTrayGrabOmni.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Autonomous(name="AutonomousTrayGrabOmni", group="Autonomous")
+public class AutonomousTrayGrabOmni extends LinearOpMode {
+
+   DcMotor leftWheel;
+   DcMotor rightwheel;
+   Servo trayGrab1;
+   Servo trayGrab2;
+
+
+
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        leftWheel = hardwareMap.dcMotor.get("left_wheel");
+        rightwheel = hardwareMap.dcMotor.get("right_wheel");
+        trayGrab1 = hardwareMap.servo.get("tray_grab_1");
+        trayGrab2 = hardwareMap.servo.get("tray_grab_2");
+        leftWheel.setDirection(DcMotor.Direction.REVERSE);
+
+        trayGrab1.setPosition(0);
+        trayGrab2.setPosition(0);
+
+        waitForStart();
+
+        leftWheel.setPower(1);
+        rightwheel.setPower(1);
+
+        sleep(1300);
+
+        leftWheel.setPower(0);
+        rightwheel.setPower(0);
+
+        trayGrab1.setPosition(0.5);
+        trayGrab2.setPosition(0.5);
+
+        leftWheel.setPower(0.6);
+        rightwheel.setPower(-0.6);
+
+        sleep(500);
+
+        leftWheel.setPower(-1);
+        rightwheel.setPower(-1);
+
+        sleep(1300);
+
+
+
+
+
+
+
+    }
+}
