388e4ced31968dff6f05f39b23414060bd59a9ae
Eugen Zdrincu <eugenzdrincu7@gmail.com>
2020-02-20 12:45:13 +0200

    added 3 autonomous tests

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoEncoderTest2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoEncoderTest2.java
@@ -4,8 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@Autonomous(name="ENCDR_TST1", group="")
-public class AutoEncoderTest extends LinearOpMode {
+@Autonomous(name="ENCDR_TST2", group="")
+public class AutoEncoderTest2 extends LinearOpMode {
 
     private DcMotor leftFrontMotor = null;
     private DcMotor rightFrontMotor = null;
@@ -22,20 +22,20 @@ public class AutoEncoderTest extends LinearOpMode {
 
         waitForStart();
 
-        leftFrontMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightFrontMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        leftRearMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightRearMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFrontMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftRearMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightFrontMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightRearMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         leftFrontMotor.setTargetPosition(1680);
         leftRearMotor.setTargetPosition(1680);
         rightFrontMotor.setTargetPosition(1680);
         rightRearMotor.setTargetPosition(1680);
 
-        leftFrontMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        leftRearMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightFrontMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightRearMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftFrontMotor.setPower(1);
+        leftRearMotor.setPower(1);
+        rightFrontMotor.setPower(1);
+        rightRearMotor.setPower(1);
 
         telemetry.addData("Lf", leftFrontMotor.getCurrentPosition());
         telemetry.addData("Rf", rightFrontMotor.getCurrentPosition());
@@ -43,6 +43,7 @@ public class AutoEncoderTest extends LinearOpMode {
         telemetry.addData("Rr", rightRearMotor.getCurrentPosition());
 
         telemetry.update();
+
     }
 
 }
